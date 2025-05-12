664840e6f76ab15bb83eaca2e5c2c64d4a3e3394
cmaina <cmaina2@my.centennialcollege.ca>
2025-05-12 01:30:47 -0400

    Your commit message here

---
+++ src/MainRouter.jsx
@@ -0,0 +1,13 @@
+import { Routes,Route } from "react-router-dom"
+
+const MainRouter = () => 
+    {
+       return(
+        <div>
+            <Routes>
+                <Route path ="/" element = {<h1>Home</h1>}/>
+            </Routes>
+        </div>
+       )
+    } 
+    export default MainRouter
