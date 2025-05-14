d2eee9ffd7fe2d11c8983bb6cf69e541ad42afdb
cmaina <cmaina2@my.centennialcollege.ca>
2025-05-14 04:33:54 -0400

    Renamed the image to integarrtiob

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -10,6 +10,13 @@ export default function Projects()
             <p><strong>Role:</strong>Developer</p>
             <p>This portfolio was build using React to showcase my work and skills.It has 5 pages, Home,About,Projects,Service and contacts.</p>
           </div>
+          {/*project 2*/}
+          <div className="project">
+            <img src="EnterpriseSystemIntegratio.jpg" alt="React Portfolio" width="300" />
+            <h2>React Portfolio</h2>
+            <p><strong>Role:</strong>Developer</p>
+            <p>This portfolio was build using React to showcase my work and skills.It has 5 pages, Home,About,Projects,Service and contacts.</p>
+          </div>
           
       </div>
        
