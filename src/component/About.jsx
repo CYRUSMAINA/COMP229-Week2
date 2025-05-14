862da80b7c395563db8befdf11f3d041745588f5
cmaina <cmaina2@my.centennialcollege.ca>
2025-05-14 04:25:48 -0400

    addded 2 project images at Public folder

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,17 +1,17 @@
-import profilePic from "./assets/Profile.jpg"
+import Profile from "../assets/Profile.jpg";
 export default function About()
 {
      return(
       <div style={{padding:'1rem',maxWidth:'600px',margin:'auto'}}>
          <h1>About Me</h1>
          <img 
-         src={profilePic}
+         src={Profile}
          alt="Profile of Cyrus Maina"
          style={{width:'150px',borderRadius:'8px',marginBottom:'1rem'}}
 
          />
          <h2>Cyrus Ciira Maina</h2>
-         <p> I am a personate student, dedicated to develop my  web application skills in react,javascript  and full stack development.Aims at creating a user friendly web.</p>
+         <p> I am a passonate student, dedicated to developing my  web application skills in React,Javascript  and full stack development.I aim to create a clean and user-friendly web experiences.</p>
          </div>
        
      )
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -3,7 +3,14 @@ export default function Projects()
      return(
       <div>
           <h1>Projects</h1>
-          <p> The first project is called react-portfolio</p>
+          {/*project 1*/}
+          <div className="project">
+            <img src="react.svg" alt="React Portfolio" width="300" />
+            <h2>React Portfolio</h2>
+            <p><strong>Role:</strong>Developer</p>
+            <p>This portfolio was build using React to showcase my work and skills.It has 5 pages, Home,About,Projects,Service and contacts.</p>
+          </div>
+          
       </div>
        
      )
