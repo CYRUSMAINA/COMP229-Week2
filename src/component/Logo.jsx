94309f044e809e5b183307cd5290115246bd60ee
cmaina <cmaina2@my.centennialcollege.ca>
2025-06-17 02:58:34 -0400

    update logo

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -9,18 +9,9 @@
 export default function Logo()
 {
      return(
-      <div style={{
-          width: '50px',
-          height: '50px',
-          backgroundColor: '#4CAF50',
-          color:'white',
-          fontWeight:'bold',
-          display:'flex',
-          alignItems:'center',
-          justifyContent:'center',
-          fontSize:'1.2rem'
-        }}>
-          CiM
+      <div className="Logo-box"
+      >
+          CM@
        </div>
        
      )
