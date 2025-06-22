b5d3b67e421444d0b74417829919aff934624017
cmaina <cmaina2@my.centennialcollege.ca>
2025-06-22 00:32:05 -0400

    logo.jsx update

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -9,9 +9,26 @@
 export default function Logo()
 {
      return(
-      <div className="logo-box"
-      >
-          CM@
+      <div className="logo-box">
+          <span className="textNfontBold">
+               CM
+          </span>
+
+          <svg 
+          className="testTeal"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2">
+               
+               <path  
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2 12h3l2-5 4 10 3-7 2 5h4"/>
+
+          </svg>
+      
        </div>
        
      )
