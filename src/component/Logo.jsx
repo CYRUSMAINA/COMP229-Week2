f681ec82aac1b0c9285b158d82d6ec075d89d8e9
cmaina <cmaina2@my.centennialcollege.ca>
2025-06-22 00:52:36 -0400

    upd logo

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -10,22 +10,33 @@ export default function Logo()
 {
      return(
       <div className="logo-box">
-          <span className="textNfontBold">
-               CM
-          </span>
+         
 
           <svg 
           className="testTeal"
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2">
-               
+          >
+               <circle cx="50" cy="50" r="45" fill="white" stroke="black" strokeWidth="4"/>
+
+               <text
+               x="50" 
+               y="55" 
+               textAnchor="middle" 
+               fontSize="20" 
+               fill="black" 
+               fontWeight="bold"
+               fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
+               > CM </text>
+
+
                <path  
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2 12h3l2-5 4 10 3-7 2 5h4"/>
+               strokeLinecap="round"
+               strokeLinejoin="round"
+               d="M15 50h15l10-15 10 30 10-20 10 10h15"
+               stroke="black"
+               strokeWidth="2"
+               fill="none"/>
 
           </svg>
       
